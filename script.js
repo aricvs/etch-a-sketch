ffce72a9ffab3af33377d3a27e4507719381838e
Ariel Santos <aricvs@gmail.com>
2023-10-25 20:45:47 -0300

    Upgrade algorithm to create exact number of cells

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let gridSize = 16;
 const gridContainer = document.querySelector(".grid-container");
 
 for (let i = 0; i < gridSize; i++) {
-  const gridCell = document.createElement("div");
-  gridCell.setAttribute("class", "grid-cell");
-  gridContainer.appendChild(gridCell);
+  for (let j = 0; j < gridSize; j++) {
+    const gridCell = document.createElement("div");
+    gridCell.setAttribute("class", "grid-cell");
+    gridContainer.appendChild(gridCell);
+  }
 }
