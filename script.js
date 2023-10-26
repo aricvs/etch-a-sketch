73c770f44cb1669d17daed80c6ec32a12c25bb32
Ariel Santos <aricvs@gmail.com>
2023-10-26 09:20:42 -0300

    Put grid generator inside a function

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-let gridSize = 16;
-const gridContainer = document.querySelector(".grid-container");
+function generateGrid(gridSize) {
+  const gridContainer = document.querySelector(".grid-container");
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-  const gridCell = document.createElement("div");
-  gridCell.setAttribute("class", "grid-cell");
-  gridContainer.appendChild(gridCell);
-  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const gridCell = document.createElement("div");
+    gridCell.setAttribute("class", "grid-cell");
+    gridContainer.appendChild(gridCell);
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  }
 }
+
+generateGrid(16);
