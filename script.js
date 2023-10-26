fc5362598765ada6c7d6b09b009c3d7eec38e6d3
Ariel Santos <aricvs@gmail.com>
2023-10-26 19:58:11 -0300

    Set up randown color picker function

--- script.js
+++ script.js
@@ -63,6 +63,11 @@ function rainbowModeToggle() {
   });
 }
 
+function rainbowColor() {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = "#" + randomColor;
+}
+
 // TODO: add a progressive color button
 
 generateGrid(16);
