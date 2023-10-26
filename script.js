58e63493ae6096e4aafcdfa9a99f16ca5c60415b
Ariel Santos <aricvs@gmail.com>
2023-10-26 09:07:32 -0300

    Simplify div generation script

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 let gridSize = 16;
 const gridContainer = document.querySelector(".grid-container");
 
-for (let i = 0; i < gridSize; i++) {
-  for (let j = 0; j < gridSize; j++) {
-    const gridCell = document.createElement("div");
-    gridCell.setAttribute("class", "grid-cell");
-    gridContainer.appendChild(gridCell);
-  }
+for (let i = 0; i < gridSize * gridSize; i++) {
+  const gridCell = document.createElement("div");
+  gridCell.setAttribute("class", "grid-cell");
+  gridContainer.appendChild(gridCell);
 }
