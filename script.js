158da1d7f3d2cb2a2c052f931bd07fc554522ce9
Ariel Santos <aricvs@gmail.com>
2023-10-26 09:15:57 -0300

    Add grid size to CSS via JS

--- script.js
+++ script.js
@@ -5,4 +5,5 @@ for (let i = 0; i < gridSize * gridSize; i++) {
   const gridCell = document.createElement("div");
   gridCell.setAttribute("class", "grid-cell");
   gridContainer.appendChild(gridCell);
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 }
